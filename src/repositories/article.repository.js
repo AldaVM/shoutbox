3fa1dfd4bd0a81f20d8565f3fc7153f9607728a9
Aldair Valencia <aldairvm95@gmail.com>
2020-05-14 16:33:24 -0500

    added custom methods a the service Article

--- src/repositories/article.repository.js
+++ src/repositories/article.repository.js
@@ -1,6 +1,6 @@
 const BaseRepository = require("./base.repository");
 const { ArticleModel } = require("../models");
-
+const { timeReading } = require("../helpers");
 let _articleModel = null;
 
 class ArticleRepository extends BaseRepository {
@@ -11,6 +11,14 @@ class ArticleRepository extends BaseRepository {
 
   async getUserArticles(author) {
     return await _articleModel.find({ author });
+  }
+
+  async create(entity) {
+    const article = new this.model(entity);
+    article.publish_time = article._id.getTimestamp();
+    article.time_reading = timeReading(entity.content);
+
+    return await article.save();
   }
 }
 
