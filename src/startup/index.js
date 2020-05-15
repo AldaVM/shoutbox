7de407ce252d38f59806069d8c679594cc8ff198
Aldair Valencia <aldairvm95@gmail.com>
2020-05-14 22:09:30 -0500

    modify console log start init

--- src/startup/index.js
+++ src/startup/index.js
@@ -3,9 +3,7 @@ const app = require("./app");
 
 function startServer() {
   app.listen(app.get("port"));
-  console.log(
-    `${config.APPLICATION_NAME} corriendo en el puerto ${app.get("port")}`
-  );
+  console.log(`${config.APPLICATION_NAME} running in port ${app.get("port")}`);
 }
 
 module.exports = startServer;
