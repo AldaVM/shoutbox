aaa79de5eef86c957e6381368ede411b7aad3589
Aldair Valencia <aldairvm95@gmail.com>
2020-05-14 19:56:03 -0500

    added environment variable for generation token

--- src/config/index.js
+++ src/config/index.js
@@ -6,4 +6,5 @@ if (process.env.NODE_ENV !== "production") {
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   APPLICATION_NAME: process.env.APPLICATION_NAME,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
