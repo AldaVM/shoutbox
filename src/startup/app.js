847ee8fe9cec41e2331449921ce34fc843bc139d
Aldair Valencia <a1214073@idat.edu.pe>
2020-05-16 18:12:48 -0500

    added cors

--- src/startup/app.js
+++ src/startup/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const routes = require("../routes/");
 const { genericError, routeNoFound } = require("../middlewares");
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.set("port", process.env.PORT || 8000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 //Config development
 if (process.env.NODE_ENV !== "production") {
